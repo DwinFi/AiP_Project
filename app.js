7c8ff1edc1da7f068d424020c5df93ba1b209dfd
DwinFi <sengerou@gmail.com>
2025-12-17 16:32:45 +0300

    9.1

--- app.js
+++ app.js
@@ -7,17 +7,17 @@ var logger = require('morgan');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/aviationDB');
 
+var session = require('express-session');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var aircraftsRouter = require('./routes/aircrafts');
 
 var app = express();
 
-// view engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// layouts
 var expressLayouts = require('express-ejs-layouts');
 app.use(expressLayouts);
 app.set('layout', 'layout');
@@ -26,22 +26,29 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(session({
+  secret: 'AviationSecret',
+  cookie: { maxAge: 60 * 1000 },
+  resave: true,
+  saveUninitialized: true
+}));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-// routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/aircrafts', aircraftsRouter);
 
-// 404
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
   res.status(err.status || 500);
-  res.render('error', { message: err.message });
+  res.render('error');
 });
 
 module.exports = app;
