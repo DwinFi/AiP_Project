9f7cfbc67fb9c5caa9cfaf2a22dd471ce607eb11
DwinFi <sengerou@gmail.com>
2025-12-18 18:19:42 +0300

    9.3

--- app.js
+++ app.js
@@ -1,54 +1,47 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const expressLayouts = require('express-ejs-layouts');
 
-var mongoose = require('mongoose');
+const indexRouter = require('./routes/index');
+const aircraftsRouter = require('./routes/aircrafts');
+
+const app = express();
+
 mongoose.connect('mongodb://127.0.0.1:27017/aviationDB');
 
-var session = require('express-session');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var aircraftsRouter = require('./routes/aircrafts');
-
-var app = express();
+mongoose.connection.on('connected', () => {
+    console.log('MongoDB connected');
+});
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
-var expressLayouts = require('express-ejs-layouts');
 app.use(expressLayouts);
 app.set('layout', 'layout');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-app.use(session({
-  secret: 'AviationSecret',
-  cookie: { maxAge: 60 * 1000 },
-  resave: true,
-  saveUninitialized: true
-}));
-
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(
+    session({
+        name: 'connect.sid',
+        secret: 'secret_key',
+        resave: false,
+        saveUninitialized: false,
+        store: new MongoStore({
+            mongooseConnection: mongoose.connection,
+            collection: 'sessions'
+        }),
+        cookie: {
+            maxAge: 1000 * 60 * 60
+        }
+    })
+);
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/aircrafts', aircraftsRouter);
-
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-app.use(function(err, req, res, next) {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
