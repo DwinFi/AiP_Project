91e8c2a2cb0d77377f16b9c9eb1432e43c4d874f
DwinFi <sengerou@gmail.com>
2025-12-20 15:12:29 +0300

    10.2

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* Страница регистрации */
 router.get('/register', function (req, res) {
     res.render('register', {
         title: 'Регистрация'
     });
+});
+
+router.post('/register', function (req, res) {
+
+    const { username, email, password, password2 } = req.body;
+
+    console.log('--- Registration form data ---');
+    console.log('Username:', username);
+    console.log('Email:', email);
+    console.log('Password:', password);
+    console.log('Repeat password:', password2);
+    console.log('------------------------------');
+
+    res.redirect('/register');
 });
 
 module.exports = router;
