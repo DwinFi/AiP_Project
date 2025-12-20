ec7b3c0403637338cbdcec6c0d5f7d7db30452dc
DwinFi <sengerou@gmail.com>
2025-12-20 14:30:03 +0300

    9.5

--- app.js
+++ app.js
@@ -4,27 +4,41 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 const expressLayouts = require('express-ejs-layouts');
+const cookieParser = require('cookie-parser');
 
 const indexRouter = require('./routes/index');
 const aircraftsRouter = require('./routes/aircrafts');
 
 const app = express();
 
+/* =======================
+   MongoDB connection
+   ======================= */
 mongoose.connect('mongodb://127.0.0.1:27017/aviationDB');
 
 mongoose.connection.on('connected', () => {
     console.log('MongoDB connected');
 });
 
+/* =======================
+   View engine
+   ======================= */
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
 app.set('layout', 'layout');
 
+/* =======================
+   Middleware
+   ======================= */
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+/* =======================
+   Session (MongoDB store)
+   ======================= */
 app.use(
     session({
         name: 'connect.sid',
@@ -41,7 +55,22 @@ app.use(
     })
 );
 
+/* =======================
+   Global variables (9.5)
+   ======================= */
+app.use(function (req, res, next) {
+    res.locals.counter = req.session.counter || 0;
+    res.locals.greeting = req.cookies.greeting || '';
+    next();
+});
+
+/* =======================
+   Routes
+   ======================= */
 app.use('/', indexRouter);
 app.use('/aircrafts', aircraftsRouter);
 
+/* =======================
+   Export
+   ======================= */
 module.exports = app;
