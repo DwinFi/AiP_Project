859433773c9033ab58e72a84210ea93426a014c2
DwinFi <sengerou@gmail.com>
2025-12-22 15:56:43 +0300

    10.8

--- routes/aircrafts.js
+++ routes/aircrafts.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var Aircraft = require('../models/Aircraft');
+var checkAuth = require('../middlewares/checkAuth');
 
 router.get('/', function(req, res) {
   res.send('Маршрутизатор самолётов работает');
 });
 
-router.get('/:nick', async function(req, res, next) {
+router.get('/:nick', checkAuth, async function(req, res, next) {
   try {
     const aircraft = await Aircraft.findOne({ nick: req.params.nick });
 
@@ -14,12 +15,12 @@ router.get('/:nick', async function(req, res, next) {
       return next(new Error('Самолёт не найден'));
     }
 
-    res.render('aircraft', {
+    res.render('plane', {
       title: aircraft.title,
-      avatar: aircraft.avatar,
+      picture: aircraft.avatar,
       desc: aircraft.desc,
-      flightSpecs: aircraft.flightSpecs,
-      technical: aircraft.technical,
+      flight: aircraft.flightSpecs,
+      tech: aircraft.technical,
       armament: aircraft.armament
     });
 
